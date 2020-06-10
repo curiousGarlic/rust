6cf5fae9d616689c4bba9c342a6b4761f706f0ee
Curious Garlic <curiousgarlic@gmail.com>
2020-06-11 00:05:21 +0300

    1st commit

---
+++ guessing_game/target/rls/debug/deps/guessing_game-c097e89ccae96546.d
@@ -0,0 +1,5 @@
+/Users/jamshidazizov/Git/rust/guessing_game/target/rls/debug/deps/guessing_game-c097e89ccae96546.rmeta: src/main.rs
+
+/Users/jamshidazizov/Git/rust/guessing_game/target/rls/debug/deps/guessing_game-c097e89ccae96546.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/deps/hello_cargo-eba0c14727ebd0a8.d
@@ -0,0 +1,5 @@
+/Users/jamshidazizov/Git/rust_projects/hello_cargo/target/debug/deps/hello_cargo-eba0c14727ebd0a8.rmeta: src/main.rs
+
+/Users/jamshidazizov/Git/rust_projects/hello_cargo/target/debug/deps/hello_cargo-eba0c14727ebd0a8.d: src/main.rs
+
+src/main.rs:
