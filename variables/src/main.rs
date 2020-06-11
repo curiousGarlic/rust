99e3247a51552ab90a12b28b8202fac7440ff8f7
Curious Garlic <curiousgarlic@gmail.com>
2020-06-11 14:37:33 +0300

    variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -23,4 +23,30 @@ fn main() {
     let spaces = "    ";
     let spaces = spaces.len();
     println!("# of spaces:    {}", spaces);
+
+    println!("___");
+    println!("//floating-point numbers");
+
+    let z = 2.0; // f64
+    let y: f32 = 3.0; // f32
+    println!("z -> {} and y -> {}", z, y);
+
+    println!("___");
+    println!("//compound types - tuples");
+    let tup = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("Value of tup index 0 is: {}", tup.0);
+    println!("The value of y is: {}", y);
+
+    println!("___");
+    println!("//compound types - arrays");
+    let months = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"];
+    println!("First month:    {}", months[0]);
+    
+    let a: [i32; 5] = [1,2,3,4,5];
+    println!("Last array item:    {}", a[4]);
+
+    println!("___");
+    println!("//functions");
 }
